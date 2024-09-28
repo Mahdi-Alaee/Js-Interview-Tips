bff50fc65537dc759c99b9b97a342c5cd9596383
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-09-28 14:45:47 +0330

    practice 15 : show data in dom

--- practice15_get-and-show-products/app.js
+++ practice15_get-and-show-products/app.js
@@ -1,6 +1,25 @@
 window.addEventListener("load", async () => {
   const mainUrl = "https://fakestoreapi.com/";
+  const container = document.querySelector(".container");
 
-  const res = await fetch(mainUrl + "products");
+  const res = await fetch(mainUrl + "products?limit=5");
   const data = await res.json();
+  console.log(data);
+  data.forEach((item) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div id="product" class="p-2 shadow-2xl bg-white rounded-md text-center">
+            <!-- title -->
+             <h3>
+                ${item.title.slice(0,20)}
+             </h3>
+             <!-- price -->
+              <p>
+                ${item.price}$
+              </p>
+        </div>
+        `
+    );
+  });
 });
