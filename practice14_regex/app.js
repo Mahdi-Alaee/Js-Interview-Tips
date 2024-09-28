4867a4d69a8dc926b2eb43c5012b393aa72153f4
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-09-28 12:32:11 +0330

    practice 14 : regex

---
+++ practice14_regex/app.js
@@ -0,0 +1,36 @@
+//! Q1 (Correct ways of creating a regular expression in javascript?)
+
+//* const email = /a/
+//* const password = new RegExp();
+
+//! Q2 (Which regex flag should be used to do a case-insensitive search?) 
+//* i
+
+
+//! Q3 (What will the following code output to the console?)
+//* console.log(/xy?z/.test('xyyz')); // xyz xz => false
+
+//! Q4 (In regular expression, what is \s ?) => space character
+
+
+//! Q5 (What will the following code output to the console?)
+// console.log(/ab*c/.test("ac")); => true
+
+//! Q6 (What will the following code output to the console?)
+// var str = 'count from 1 to 10'
+// console.log(str.match(/[0-9]+/)); // ['1']
+
+//! Q7 (What will the following code output to the console?)
+// console.log("abc".match(/[a-z]/));
+// console.log("123".match(/[a-z]/));
+
+//! Q8 (Which character is the flag?)
+// var x = /ab+\d/i // i
+
+
+// Q9 (What is \d in RegEx?) // digit character (Number 0 - 9)
+
+
+// Q10 (What will the following code output to the console?)
+// console.log(/ab+c/.test('abc')); // true
+
