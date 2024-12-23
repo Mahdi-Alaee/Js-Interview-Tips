782e4f9e98a6fbcf65cf8c35ce674b52c918c709
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-12-23 15:44:56 +0330

    isNaN vs Number.isNaN

---
+++ practice17_isNaN-vs-Number.isNaN/app.js
@@ -0,0 +1,8 @@
+console.log(isNaN(12)); // false
+console.log(isNaN('12')); // false
+console.log(isNaN('mahdi')); // true
+console.log('----------------------------------');
+console.log(Number.isNaN(12)); // false
+console.log(Number.isNaN('12')); // false
+console.log(Number.isNaN('mahdi')); // false
+console.log(Number.isNaN(NaN)); // true
